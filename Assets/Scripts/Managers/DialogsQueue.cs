1d6fbd536ecf7d7b869de20d0c74be8e235a6770
georgy.polishchuk <georgy.polishchuk@kosmosteam.com>
2022-09-15 15:35:36 +0300

    Custom QueuePos drawer

--- Assets/Scripts/Managers/DialogsQueue.cs
+++ Assets/Scripts/Managers/DialogsQueue.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -125,16 +126,47 @@ public class DialogsQueue
 }
 
 [System.Serializable]
-public class QueuePos
-{
-    public string name;
+public class QueuePos {
     public Dialog dialog;
     public float fromTime, toTime;
-    public int priority = 0;
 
-    public bool IsTimeWithinRange(float time)
-    {
+    public bool IsTimeWithinRange(float time) => fromTime <= time && time <= toTime;
+}
 
-        return fromTime <= time && time <= toTime;
+[CustomPropertyDrawer(typeof(QueuePos))]
+public class QueuePosDrawer : PropertyDrawer {
+    private const int dialogSize = 200;
+    private const int fromTimeSize = 50;
+    private const int toTimeSize = 50;
+    private const int spaceSize = 20;
+
+    // Draw the property inside the given rect
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        // Using BeginProperty / EndProperty on the parent property means that
+        // prefab override logic works on the entire property.
+        EditorGUI.BeginProperty(position, label, property);
+
+        // Don't make child fields be indented
+        var indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+
+        // Calculate rects
+        Rect dialogRect = new Rect(position.x, position.y, dialogSize, position.height);
+        Rect fromTimeRect = new Rect(position.x, position.y, fromTimeSize, position.height);
+        fromTimeRect.x += dialogSize + spaceSize;
+        Rect toTimeRect = new Rect(position.x, position.y, toTimeSize, position.height);
+        toTimeRect.x += fromTimeSize + dialogSize + spaceSize * 2;
+
+        // Draw fields - passs GUIContent.none to each so they are drawn without labels
+        
+        EditorGUI.PropertyField(dialogRect, property.FindPropertyRelative("dialog"), GUIContent.none);
+        EditorGUI.PropertyField(fromTimeRect, property.FindPropertyRelative("fromTime"), GUIContent.none);
+        EditorGUI.PropertyField(toTimeRect, property.FindPropertyRelative("toTime"), GUIContent.none);
+
+        // Set indent back to what it was
+        EditorGUI.indentLevel = indent;
+
+        EditorGUI.EndProperty();
     }
 }
+    
